99d755acd72bdb9b8c4a08d41ad45056bdcef8e6
lenkim <joenggyu0@gmail.com>
2021-07-16 14:45:28 +0900

    feat: add DataJdbcConfig

---
+++ src/main/java/kitchenpos/repository/config/DataJdbcConfig.java
@@ -0,0 +1,19 @@
+package kitchenpos.repository.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jdbc.core.convert.JdbcCustomConversions;
+import org.springframework.data.jdbc.repository.config.AbstractJdbcConfiguration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DataJdbcConfig extends AbstractJdbcConfiguration {
+
+    @Bean
+    public JdbcCustomConversions jdbcCustomConversions() {
+        return new JdbcCustomConversions(Arrays.asList(
+                // TODO
+        ));
+    }
+}
